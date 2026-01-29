a469e527804442406c3b74a41ccf6d43ea927feb
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-01-29 17:00:26 +0600

    live field title bar added

--- src/app/(dashboard)/live-feed/page.tsx
+++ src/app/(dashboard)/live-feed/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { ChartSpline, Plus } from "lucide-react";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="space-y-10">
+      <div className="flex justify-between items-center">
+        <h1 className="flex items-center font-bold gap-2 text-xl">
+          <span>
+            <ChartSpline className="h-5 w-5" />
+          </span>
+          <span>Live Feed</span>
+        </h1>
 
-export default page
+        <button className="flex items-center font-bold gap-2 bg-gradient-to-r from-[#329150] to-[#0d4728] hover:bg-gradient-to-r hover:from-[#0d4728] hover:to-[#329150] py-3 px-4 rounded-3xl">
+          <span>
+            <Plus></Plus>
+          </span>
+
+          <span>New Pick</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default page;
