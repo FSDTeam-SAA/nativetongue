bf628fb1d2290cb07e4d650c44e73d2791be2f75
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-01-29 19:17:15 +0600

    sidebar hover done

--- src/app/(dashboard)/_components/app-sidebar.tsx
+++ src/app/(dashboard)/_components/app-sidebar.tsx
@@ -1,108 +1,3 @@
-// "use client";
-// import {
-//   ChartSpline,
-//   Dumbbell,
-//   LayoutDashboard,
-//   Settings,
-//   Trophy,
-//   Users,
-// } from "lucide-react";
-
-// import {
-//   Sidebar,
-//   SidebarContent,
-//   SidebarGroup,
-//   SidebarGroupContent,
-//   SidebarGroupLabel,
-//   SidebarMenu,
-//   SidebarMenuButton,
-//   SidebarMenuItem,
-// } from "@/components/ui/sidebar";
-// import Image from "next/image";
-// import Link from "next/link";
-
-// import { usePathname } from "next/navigation";
-
-// // Menu items.
-// const items = [
-//   {
-//     title: "Dashboard",
-//     url: "/",
-//     icon: LayoutDashboard,
-//   },
-//   {
-//     title: "Live Feed",
-//     url: "/live-feed",
-//     icon: ChartSpline,
-//   },
-//   {
-//     title: "Leaderboard",
-//     url: "/leaderboard",
-//     icon: Trophy,
-//   },
-//   {
-//     title: "Sports",
-//     url: "/sports",
-//     icon: Dumbbell,
-//   },
-//   {
-//     title: "Community",
-//     url: "/community",
-//     icon: Users,
-//   },
-//   {
-//     title: "Settings",
-//     url: "/settings",
-//     icon: Settings,
-//   },
-// ];
-
-// export function AppSidebar() {
-//   const pathName = usePathname();
-
-//   return (
-//     <Sidebar>
-//       <SidebarContent className="bg-black text-white ">
-//         <SidebarGroup>
-//           <SidebarGroupLabel>
-//             <Link href={`/`}>
-//               <Image
-//                 src={`/logo.png`}
-//                 alt="img.png"
-//                 width={1000}
-//                 height={1000}
-//                 className="object-cover mt-16"
-//               />
-//             </Link>
-//           </SidebarGroupLabel>
-
-//           <SidebarGroupContent className="mt-16">
-//             <SidebarMenu>
-//               {items.map((item) => {
-//                 const isActive = pathName === item.url;
-//                 return (
-//                   <SidebarMenuItem key={item.title}>
-//                     <SidebarMenuButton asChild>
-//                       <Link
-//                         href={item.url}
-//                         className={`h-[45px] hover:text-white  font-bold ${isActive ? "bg-gradient-to-r from-[#329150] to-[#0d4728] hover:bg-gradient-to-r hover:from-[#0d4728] hover:to-[#329150]" : "hover:bg-gradient-to-r hover:from-[#329150] hover:to-[#0d4728]"}`}
-//                       >
-//                         <item.icon />
-//                         <span>{item.title}</span>
-//                       </Link>
-//                     </SidebarMenuButton>
-//                   </SidebarMenuItem>
-//                 );
-//               })}
-//             </SidebarMenu>
-//           </SidebarGroupContent>
-//         </SidebarGroup>
-//       </SidebarContent>
-//     </Sidebar>
-//   );
-// }
-
-
 "use client";
 import {
   ChartSpline,
@@ -193,10 +88,11 @@ export function AppSidebar() {
                       <SidebarMenuButton asChild>
                         <Link
                           href={item.url}
-                          className={`h-[45px] flex items-center gap-2 px-4 font-bold ${isActive
-                            ? "bg-gradient-to-r from-[#329150] to-[#0d4728] hover:bg-gradient-to-r hover:from-[#0d4728] hover:to-[#329150]"
-                            : "hover:bg-gradient-to-r hover:from-[#329150] hover:to-[#0d4728]"
-                            }`}
+                          className={`h-[45px] flex items-center gap-2 px-4 font-bold ${
+                            isActive
+                              ? "bg-gradient-to-r from-[#329150] to-[#0d4728] hover:bg-gradient-to-r hover:from-[#0d4728] hover:to-[#329150] hover:text-white"
+                              : "hover:bg-gradient-to-r hover:from-[#329150] hover:to-[#0d4728] hover:text-white"
+                          }`}
                         >
                           <item.icon />
                           <span>{item.title}</span>
@@ -228,7 +124,10 @@ export function AppSidebar() {
           </div>
 
           {/* Logout Button */}
-          <button onClick={()=>signOut({callbackUrl:"/login"})} className="w-full flex items-center gap-2 text-[#E53838] border border-[#E53838] px-4 py-2 rounded-lg hover:bg-[#E53838]/60 hover:text-white transition">
+          <button
+            onClick={() => signOut({ callbackUrl: "/login" })}
+            className="w-full flex items-center gap-2 text-[#E53838] border border-[#E53838] px-4 py-2 rounded-lg hover:bg-[#E53838]/60 hover:text-white transition"
+          >
             <LogOut size={18} />
             Log out
           </button>
